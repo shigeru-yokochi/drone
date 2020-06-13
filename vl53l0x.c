d6a387b64a519e01bc3e5d5f8d7412ce6dd73e1b
shigeru-yokochi <shigeru@yokochi.jp>
2020-06-13 19:52:22 +0900

    ;

--- vl53l0x.c
+++ vl53l0x.c
@@ -57,11 +57,10 @@ printf("---------- code:%d\n",VL53L0X_SetDeviceAddress(m_pMyDevice,	m_pMyDevice-
  
    sleep(1);
  
-    m_pMyDevice->fd = VL53L0X_i2c_init("/dev/i2c-1", m_pMyDevice->I2cDevAddr); //choose between i2c-0 and i2c-1; On the raspberry pi zero, i2c-1 are pins 2 and 3
-  sleep(1);
   digitalWrite(20,HIGH);
    sleep(1);
- 
+   m_pMyDevice->fd = VL53L0X_i2c_init("/dev/i2c-1", m_pMyDevice->I2cDevAddr); //choose between i2c-0 and i2c-1; On the raspberry pi zero, i2c-1 are pins 2 and 3
+  
 
 //    if (MyDevice.fd<0) {
     if (m_pMyDevice->fd<0) {
