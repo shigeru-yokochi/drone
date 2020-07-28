259b757946199abc60244c37b6c3e405edcc007e
shigeru-yokochi <shigeru@yokochi.jp>
2020-07-28 18:50:48 +0900

    debug

--- main.c
+++ main.c
@@ -112,7 +112,7 @@ static FILE *m_fp,*m_fpVL53L0X;
 #define MINIMUM_GROUND_CLEARANCE	40	//最小地上高
 #define MAXIMUM_GROUND_CLEARANCE	500	//最大地上高(できるだけ高くするmax500目標)
 
-#define DEBUG_MAINLOOP_TO			6	//デバッグ用メインループタイムアウト指定(sec)
+#define DEBUG_MAINLOOP_TO			7	//デバッグ用メインループタイムアウト指定(sec)
 #define FLIGHT_TIME					4	//DEBUG_MAINLOOP_TO - FLIGHT_TIME = landing time
 #define OFFSET_POWER				650
 #define LANDING_POWER				500
