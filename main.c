f476f19c5b2b2e9797eed1ac95127f300604324f
shigeru-yokochi <shigeru@yokochi.jp>
2020-08-21 17:39:06 +0900

    debug

--- main.c
+++ main.c
@@ -112,12 +112,12 @@ static FILE *m_fp,*m_fpVL53L0X;
 #define CORRECTION_POWER_N -100	//障害物回避用の出力補正値
 
 #define MINIMUM_GROUND_CLEARANCE	40	//最小地上高
-#define MAXIMUM_GROUND_CLEARANCE	500	//最大地上高(できるだけ高くするmax500目標)
+#define MAXIMUM_GROUND_CLEARANCE	400	//最大地上高(できるだけ高くするmax500目標)
 
-#define DEBUG_MAINLOOP_TO			7	//デバッグ用メインループタイムアウト指定(sec)
-#define FLIGHT_TIME					5	//landing time = FLIGHT_TIME - DEBUG_MAINLOOP_TO
-#define TAKEOFF_POWER				600
-#define LANDING_POWER				500
+#define DEBUG_MAINLOOP_TO			6	//デバッグ用メインループタイムアウト指定(sec)
+#define FLIGHT_TIME					4	//landing time = FLIGHT_TIME - DEBUG_MAINLOOP_TO
+#define TAKEOFF_POWER				500
+#define LANDING_POWER				400
 
 
 //姿勢制御用データ格納用
