640476402a3278b22e95e2e923e9bd2c7e590621
shigeru-yokochi <shigeru@yokochi.jp>
2020-08-22 10:40:08 +0900

    debug

--- main.c
+++ main.c
@@ -120,8 +120,8 @@ static FILE *m_fp,*m_fpVL53L0X;
 #define FLIGHT_TIME					4	//DEBUG_MAINLOOP_TO - FLIGHT_TIME = landing time
 //#define CLIMB_POWER 				550
 //#define LANDING_POWER 			450
-#define CLIMB_POWER 				100	//飛ばないテスト用
-#define LANDING_POWER 				50
+#define CLIMB_POWER 				50	//飛ばないテスト用
+#define LANDING_POWER 				30
 
 
 //姿勢制御用データ格納用
