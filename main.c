3b5245c551931bfb5eabda937cf2cf93f06f2224
shigeru-yokochi <shigeru@yokochi.jp>
2020-08-22 13:09:07 +0900

    debug

--- main.c
+++ main.c
@@ -120,8 +120,8 @@ static FILE *m_fp,*m_fpVL53L0X;
 
 #define DEBUG_MAINLOOP_TO			5	//デバッグ用メインループタイムアウト指定(sec)
 #define FLIGHT_TIME					4	//DEBUG_MAINLOOP_TO - FLIGHT_TIME = landing time
-#define CLIMB_POWER 				300
-#define LANDING_POWER 				200
+#define CLIMB_POWER 				400
+#define LANDING_POWER 				300
 //#define CLIMB_POWER 				50	//飛ばないテスト用
 //#define LANDING_POWER 			30
 
